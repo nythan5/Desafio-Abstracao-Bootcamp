d811741387baaac5bc0900d29289fc6d71b6e13c
nythan5 <g.n.d2014@gmail.com>
2023-04-07 12:15:07 -0300

    Criando Classes Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descricao curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso PowerBI");
+        curso2.setDescricao("Descricao curso PowerBI");
+        curso2.setCargaHoraria(2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descricao da Mentoria");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(mentoria);
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+
+
     }
 }
